850bdaa313e911f89e4c479c8a15c6b63730159e
Carlo Field <carlo@swiss.dev>
2026-01-13 15:51:53 +0100

    feat: initial commit
    Signed-off-by: Carlo Field <carlo@swiss.dev>

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = ''
+    Vulnerable and unsafe citrix_workspace flake because citrix doesn't update their dependencies.
+
+    This is fine.
+    - Citrix, probably.
+  '';
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs-25-05.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    nixpkgs-25-05,
+  } @ inputs: {
+    overlays.default = final: prev: let
+      old = import nixpkgs-25-05 {
+        system = prev.stdenv.hostPlatform.system or prev.system;
+        config = {allowUnfree = true;};
+      };
+    in {
+      citrix_workspace = prev.citrix_workspace.overrideAttrs (oa: {
+        buildInputs = (oa.buildInputs or []) ++ [old.webkitgtk_4_0];
+        meta = (oa.meta or {}) // {broken = false;};
+      });
+    };
+  };
+}
